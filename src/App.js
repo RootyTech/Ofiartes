3a3ea8a03a258b7492f783240e46d7e75e8a6818
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-10-22 16:01:54 -0500

    Variables globales SASS + Fuentes + Rutas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React, { useEffect, useState } from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
 /** Componentes */
+import { EquipoTrabajo } from './components/EquipoTrabajo';
+import { Footer } from './components/Footer';
+import { Formulario } from './components/Formulario';
+import { Galeria } from './components/Galeria';
+import { Header } from './components/Header';
+import { MisionVision } from './components/MisionVision';
+import { Novedades } from './components/Novedades';
+import { QueComo } from './components/QueComo';
+import { Talleres } from './components/Talleres';
 import { Testimonios } from './components/Testimonios';
 
 /** Contentful */
@@ -37,6 +46,15 @@ export const App = () => {
                 { /** Lo que cambiará */}
                 <Switch>
                     <Route exact path="/testimonios" component={Testimonios} />
+                    <Route exact path="/equipo_trabajo" component={EquipoTrabajo} />
+                    <Route exact path="/footer" component={Footer} />
+                    <Route exact path="/formulario" component={Formulario} />
+                    <Route exact path="/galeria" component={Galeria} />
+                    <Route exact path="/header" component={Header} />
+                    <Route exact path="/mision_vision" component={MisionVision} />
+                    <Route exact path="/novedades" component={Novedades} />
+                    <Route exact path="/que_como" component={QueComo} />
+                    <Route exact path="/talleres" component={Talleres} />               
                 </Switch>
             </HashRouter>
         </context.Provider>
--- src/components/Testimonios/index.jsx
+++ src/components/Testimonios/index.jsx
@@ -1,13 +1,49 @@
 import React, { useContext } from 'react';
 import { Helmet } from 'react-helmet';
 import { context } from '../../context';
-import './estilos.sass';
 
 export const Testimonios = () => {
+
+    import('./estilos.sass');
+    if (window.outerWidth >= 1024) {
+        import('./desktop.sass');
+    }
+
     return (
-        <>
-        
-        </>
+        <div className="testimonios">
+            <div className="testimonios__item">
+                <div className="testimonios__item--info">
+                    <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="Imagen de ejemplo de una persona" width="150px" height="150px" />
+                    <h2>Nombre</h2>
+                    <p>Cargo</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eos rerum quis temporibus.</p>
+            </div>
+            <div className="testimonios__item">
+                <div className="testimonios__item--info">
+                    <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="Imagen de ejemplo de una persona" width="150px" height="150px" />
+                    <h2>Nombre</h2>
+                    <p>Cargo</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eos rerum quis temporibus.</p>
+            </div>
+            <div className="testimonios__item">
+                <div className="testimonios__item--info">
+                    <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="Imagen de ejemplo de una persona" width="150px" height="150px" />
+                    <h2>Nombre</h2>
+                    <p>Cargo</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eos rerum quis temporibus.</p>
+            </div>
+            <div className="testimonios__item">
+                <div className="testimonios__item--info">
+                    <img src="https://randomuser.me/api/portraits/men/88.jpg" alt="Imagen de ejemplo de una persona" width="150px" height="150px" />
+                    <h2>Nombre</h2>
+                    <p>Cargo</p>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque eos rerum quis temporibus.</p>
+            </div>
+        </div>
     )
 }
 
