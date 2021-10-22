8e35b82e953efb178212bd8f57658f9127f4f5c0
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-10-22 18:02:42 -0500

    Footer practicamente terminado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import { FaFacebookSquare, FaInstagramSquare, FaWhatsappSquare } from 'react-icons/fa';
+import { FaFacebookF, FaInstagramSquare, FaWhatsappSquare } from 'react-icons/fa';
+import { MediaQueryTablet } from '../../lib/mediaQuery';
 
 export const Footer = () => {
 
     import('./estilos.sass');
+    MediaQueryTablet() && import('./desktop.sass');
     
     return (
         <footer className="footer">
             <h2>CONTACTO</h2>
-            <div className="footer__info">
-                <p><span>Dirección:</span> calle 49 # 39-16</p>
-                <p><span>Correo:</span> corporacionofiartes@gmail.com</p>
-                <p><span>Nit:</span> 901284540-2</p>
-                <p><span>Teléfono</span> 3147477226</p>
+            <div className="footer__contenido">
+                <div className="footer__info">
+                    <p><span>Dirección:</span> Calle 49 # 39-16</p>
+                    <p><span>Correo:</span> corporacionofiartes@gmail.com</p>
+                    <p><span>Nit:</span> 901284540-2</p>
+                    <p><span>Teléfono</span> 3147477226</p>
+                </div>
+                <div className="footer__icons">
+                    <h2>Redes Sociales</h2>
+                    <div className="footer__icons--social">
+                        <FaFacebookF className="footer__icons--social--facebook"/>
+                        <FaWhatsappSquare className="footer__icons--social--whatsapp"/>
+                        <FaInstagramSquare
+                        className="footer__icons--social--instagram"/>
+                    </div>
+                </div>
             </div>
-            <h2>Redes Sociales</h2>
-            <div className="footer__redes">
-                <FaFacebookSquare className="footer__redes--facebook"/>
-                <FaWhatsappSquare className="footer__redes--whatsapp"/>
-                <FaInstagramSquare
-                className="footer__redes--instagram"/>
-            </div>
+            <h4>© Corporación ofiartes 2021</h4>
         </footer>
     )
 }
