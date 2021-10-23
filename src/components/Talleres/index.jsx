3ed08e7f0a308cbbf73e63684b606d394dd2a5a1
Isabela Ceballos <isaceballos2001@gmail.com>
2021-10-23 16:47:17 -0500

    agregando un poco de talleres

--- src/components/Talleres/index.jsx
+++ src/components/Talleres/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { context } from '../../context';
+//import { FichaTaller } from './taller'
 
+
+{/*import {taller} from '   ';*/}
 export const Talleres = () => {
+    const {talleres} = useContext(context); /*asi traigo el objeto contexto*/
     return (
         <>
-        
+        <div className="search"> {/*display flex*/}
+            <input type="text" className="search__input" placeholder="Buscar un curso" />
+            <FaSearch/>
+        </div>
+        {console.log(talleres)}
+        <section className="details__talleres">
+            {
+                talleres ?
+                talleres.map((talleres,index) => (
+                    <div key= {`taller-${index}`}>
+                        <details>
+                            <summary>{talleres.fields.title}</summary>
+                            <p>{talleres.fields.description}</p>
+                        </details>
+                        <img src="" alt="" />
+                    </div>
+                ))
+                :<h2>Loading...</h2>
+            }
+        </section>
         </>
     )
 }
