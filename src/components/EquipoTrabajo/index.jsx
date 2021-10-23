6f181b9eebd3710f4115216a39319498afb4cd89
Santiago Restrepo <santirstrp@gmail.com>
2021-10-22 22:53:23 -0500

    Terminada estructura y estilos iniciales de carousel de integrantes

--- src/components/EquipoTrabajo/index.jsx
+++ src/components/EquipoTrabajo/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Carousel } from './Carousel';
 import './estilos.sass';
-
 export const EquipoTrabajo = () => {
     return (
-        <>
-        
-        </>
+        <section className="workTeam">
+            <Carousel/>
+        </section>
     )
 }
