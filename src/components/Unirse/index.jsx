39a68bb0fa69c7246705e440ea96a92ff798aa68
Daniela Jurado Blandón <daniijuradob@gmail.com>
2021-10-23 11:06:54 -0500

    media del desktop

--- src/components/Unirse/index.jsx
+++ src/components/Unirse/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './estilos.sass';
 //importación de imagenes
 import img_beneficiario from '../../assets/img_beneficiario.png';
 import img_empresa from '../../assets/img_empresa.png';
 import img_voluntario from '../../assets/img_voluntario.png';
+import './estilos.sass';
+
 
 export const Unirse = () => {
     return (
@@ -17,21 +18,10 @@ export const Unirse = () => {
                         Si haces parte de la población discapacitada 
                         y deseas pertenecer a la corporación para el 
                         fomento al empleo y el emprendimiento esta es 
-                        tu oportunidad. <br /> Déjanos tus datos.
+                        tu oportunidad. Déjanos tus datos.
                     </p>
                     <button>
-                        <Link to="#">Quiero inscribirme como beneficiario</Link>
-                    </button>
-                </div>
-                <div className="join__wrap--item">
-                    <img src={img_empresa} alt="unete como voluntario" />
-                    <p>
-                        Esta es una gran oportunidad de trasmitir 
-                        tu tiempo y talento a las personas que más 
-                        lo necesitan. <br /> Déjanos tus datos.
-                    </p>
-                    <button>
-                        <Link to="#">Quiero ser voluntario</Link>
+                        <Link to="#">Quiero ser beneficiario</Link>
                     </button>
                 </div>
                 <div className="join__wrap--item">
@@ -42,10 +32,21 @@ export const Unirse = () => {
                         sociedad y un modo de realización personal, si 
                         tu empresa desea ser incluyente laboralmente, la 
                         corporación te brindará la asesoría adecuada en 
-                        cada uno de los procesos. <br /> Déjanos tus datos.
+                        cada uno de los procesos. Déjanos tus datos.
                     </p>
                     <button>
                         <Link to="#">Quiero ser una empresa aliada</Link>
+                    </button>
+                </div>
+                <div className="join__wrap--item">
+                    <img src={img_empresa} alt="unete como voluntario" />
+                    <p>
+                        Esta es una gran oportunidad de trasmitir 
+                        tu tiempo y talento a las personas que más 
+                        lo necesitan. Déjanos tus datos.
+                    </p>
+                    <button>
+                        <Link to="#">Quiero ser voluntario</Link>
                     </button>
                 </div>
             </section>
