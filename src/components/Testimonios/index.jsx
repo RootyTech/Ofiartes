e9cdacd294a60e57b85828148e7d09f8bafecff6
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-10-28 08:12:57 -0500

    React Skeleton + Re-render resize -> Testimonios

--- src/components/Testimonios/index.jsx
+++ src/components/Testimonios/index.jsx
@@ -6,7 +6,7 @@ import { context } from '../../context';
 /** COMPONENTES */
 import { Testimonio } from './Testimonio';
 import { Circle } from './Circle';
-import { LoaderSpinner } from '../commons/Loader';
+import { MyLoader } from './Skeleton';
 
 import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from 'react-icons/md'
 
@@ -36,12 +36,22 @@ export const Testimonios = () => {
     /** Estilos importamos de manera dinamica */
     import(/* webpackChunkName: 'testimonios-mobile' */'./estilos.sass');
     MediaQueryDesktop() && import(/* webpackChunkName: 'testimonios-desktop' */'./desktop.sass');
-
+    
     const { testimonios } = useContext(context);
-    const [ Testimonios, setTestimonios ] = useState([]);
-
+    
     const [ counter, dispatch ] = useReducer(reducerCounter, {first: 0, second: 1});
+    
+    const [ Testimonios, setTestimonios ] = useState([]);
+    const [widthSize, setWidthSize] = useState("Mobile");
 
+    window.addEventListener('resize', () => {
+        if (MediaQueryDesktop()) {
+            setWidthSize("Desktop")
+        } else {
+            setWidthSize("MobileD")
+        }
+    });
+    
     useEffect(() => {
         
         if ( testimonios ) {
@@ -64,11 +74,13 @@ export const Testimonios = () => {
         <section className="testimonios">
             <h2>Testimonios</h2>
             {
-                testimonios ?
+                Testimonios.length !== 0 ?
                 <div className="testimonios__principal">
-                    <a className="testimonios__principal--arrow" onClick={() => dispatch({type: 'Decrement', Arraylength: testimonios.length})}>
-                        <MdOutlineArrowBackIos />
-                    </a>
+                    <div>
+                        <a className="testimonios__principal--arrow" onClick={() => dispatch({type: 'Decrement', Arraylength: testimonios.length})}>
+                            <MdOutlineArrowBackIos />
+                        </a>
+                    </div>
                     <div className="testimonios__content">
                         {
                             MediaQueryDesktop() ?
@@ -79,11 +91,40 @@ export const Testimonios = () => {
                             : Testimonios[counter.first]
                         }
                     </div>
-                    <a className="testimonios__principal--arrow" onClick={() => dispatch({type: 'Increment', Arraylength: testimonios.length})}>
-                        <MdOutlineArrowForwardIos />
-                    </a>
+                    <div>
+                        <a className="testimonios__principal--arrow" onClick={() => dispatch({type: 'Increment', Arraylength: testimonios.length})}>
+                            <MdOutlineArrowForwardIos />
+                        </a>
+                    </div>
                 </div>
-                : <LoaderSpinner />
+                : 
+                <div className="testimonios__principal">
+                    <div className="testimonios__content">
+                        {
+                            MediaQueryDesktop() ?
+                            <>  
+                                <MyLoader {...{
+                                    width: 480,
+                                    height: 210,
+                                    contentwidth: 360,
+                                    xpositions: [60, 87, 114, 240],
+                                }} />
+                                <MyLoader {...{
+                                    width: 480,
+                                    height: 210,
+                                    contentwidth: 360,
+                                    xpositions: [60, 87, 114, 240],
+                                }} />
+                            </>
+                            : <MyLoader {...{
+                                width: 270,
+                                height: 235,
+                                contentwidth: 270,
+                                xpositions: [10, 30, 57, 135],
+                            }} />
+                        }
+                    </div>
+                </div>
             }
             <div className="circles">
                 {
