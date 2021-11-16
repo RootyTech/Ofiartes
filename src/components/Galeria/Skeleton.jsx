e4349a0a5cf33e259acad38c4992064e866e25c3
Daniela Jurado Blandón <daniijuradob@gmail.com>
2021-11-16 14:31:37 -0500

    galeria avances y esqueleto

---
+++ src/components/Galeria/Skeleton.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+export const LoaderGalery = (props) => (
+    <ContentLoader 
+        speed={2}
+        viewBox={`0 0 ${props.width} ${props.height}`}
+        backgroundColor="#f3f3f3"
+        foregroundColor="#ecebeb"
+        {...props}
+    >
+        <rect x={props.values.xposition} y={0} rx="0" ry="0" width={props.values.width} height={props.values.height} />
+        <rect x={ props.values.xposition + props.values.width + 5} y={0} rx="0" ry="0" width={props.values.width} height={props.values.height} />  
+        <rect x={ props.values.xposition + props.values.width*2 + 10} y={0} rx="0" ry="0" width={props.values.width} height={props.values.height} /> 
+        
+        <rect x={props.values.xposition} y={props.values.height + 5} rx="0" ry="0" width={props.values.width} height={props.values.height} />
+        <rect x={ props.values.xposition + props.values.width + 5} y={props.values.height + 5} rx="0" ry="0" width={props.values.width} height={props.values.height} />  
+        <rect x={ props.values.xposition + props.values.width*2 + 10} y={props.values.height + 5} rx="0" ry="0" width={props.values.width} height={props.values.height} /> 
+
+        <rect x={props.values.xposition} y={props.values.height*2 + 10} rx="0" ry="0" width={props.values.width} height={props.values.height} />
+        <rect x={ props.values.xposition + props.values.width + 5} y={props.values.height*2 + 10} rx="0" ry="0" width={props.values.width} height={props.values.height} />  
+        <rect x={ props.values.xposition + props.values.width*2 + 10} y={props.values.height*2 + 10} rx="0" ry="0" width={props.values.width} height={props.values.height} /> 
+
+    </ContentLoader>
+)
--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
-import './estilos.sass';
+import React, { useContext, useLayoutEffect } from 'react';
+import { MediaQueryDesktop, MediaQueryTablet } from '../../lib/mediaQuery';
+import { context } from '../../context';
+import { LoaderGalery } from './Skeleton';
 
 export const Galeria = () => {
+    import ('./estilos.sass');
+    //MediaQueryTablet() && import('./desktop.sass');
+
+    const { galeria } = useContext(context);
+
+    console.log(galeria);
+    /**Estructura objeto galeria []
+     * 0:
+     *  fields:
+     *      image:
+     *          fields:
+     *              description: ""
+     *              file:
+     *                  contentType:...
+     *                  details:...
+     *                  fileName:""
+     *                  url:"//" <---
+     *              title: "texto" <---
+     *          metadata:....
+     *          sys:.....
+     *      title: "texto" <---
+     *  metadata:...
+     *  sys:....
+     */
     return (
-        <>
-        
-        </>
+        <section className="galery">
+            <h1>Galería de evidencias</h1>
+            <section className="galery__pics">
+                {
+                    galeria ?
+                        galeria.map((galeria,i) => (
+                            <div className="galery__pics--item" key={`image_${i}`}>
+                                <img src={galeria.fields.image.fields.file.url} alt={galeria.fields.image.fields.title} />
+                                <div>{galeria.fields.title}</div>
+                            </div>
+                        ))
+                    : MediaQueryDesktop() ?
+                    <LoaderGalery {...{
+                        width: 1024,
+                        height: 600,
+                        values: {
+                            width:250,
+                            height:192,
+                            xposition:250,
+                        },
+                    }} />
+                    : MediaQueryTablet() ?
+                    <LoaderGalery {...{
+                        width: 758,
+                        height: 600,
+                        values: {
+                            width:168,
+                            height:192,
+                            xposition:50,
+                        },
+                    }} />
+                    :
+                    <LoaderGalery {...{
+                        width: 320,
+                        height: 600,
+                        values: {
+                            width:104,
+                            height:192,
+                            xposition:0,
+                        },
+                    }} />
+                }
+            </section>
+        </section>
     )
 }
