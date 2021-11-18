1697fd6b88c58e90677f40f1c81ad41b85d84c84
MiguelABoni <miguelbedoyabonilla@gmail.com>
2021-11-18 14:49:39 -0500

    Header Desktop y DesktopL LISTO ( ͡° ͜ʖ ͡°)

--- src/components/Header/infoHeader.jsx
+++ src/components/Header/infoHeader.jsx
@@ -14,15 +14,27 @@ export const InfoHeader = (props) => {
             <div className="header__top">
                 <div className="header__menu">
                     {/* AQUÍ VA A IR UN CONDICIONAL PARA SABER QUE MENÚ MOSTRAR */}
-                    {/* AQUI VA A IR EL MENÚ DE DESKTOP */}
+                    
+                    {/* MENÚ DE DESKTOP */}
                     {MediaQueryDesktop() ?
-                    <ul>
-                        <li>
-                            <Link to="#">Quienes Somos</Link>
-                        </li>
-                    </ul>
+                    <nav className="header__nav">
+                        <ul>
+                            <li>
+                                <Link to="/home">Inicio</Link>
+                            </li>
+                            <li>
+                                <Link to="/talleres">Talleres</Link>
+                            </li>
+                            <li>
+                                <Link to="/quienesSomos">Quienes Somos</Link>
+                            </li>
+                            <li>
+                                <Link to="/contacto">Contacto</Link>
+                            </li>
+                        </ul>
+                    </nav>
                     :
-                    <Link to="#" className="header__menu--on">
+                    <Link to="#" className="header__menu--on" replace>
                         <span></span>
                         <span></span>
                         <span></span>
@@ -42,7 +54,7 @@ export const InfoHeader = (props) => {
                 {props.pagina === 'Home'
                 ? 
                 <div className="header__bottom--buttons">
-                    <Link to="#">
+                    <Link to="#" replace>
                         <ButtonFill background="green" color="white" content="DONAR" />
                     </Link>
                     <br /><br />
